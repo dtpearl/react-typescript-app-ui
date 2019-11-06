4cc26e5b6e7cae4fbce093c98fb2d99ac3cc0df2
David Pearl <davidpearl@Davids-MacBook-Air.local>
2019-11-06 14:30:00 +1100

    Import graphql-tag

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { NextPage } from 'next';
+import gql from 'graphql-tag';
+import { Query } from 'react-apollo';
 
 interface InitialProps {
     greeting: string;
@@ -8,6 +10,16 @@ interface InitialProps {
 interface Props extends InitialProps {
 
 }
+
+const tasksQuery = gql`
+query Tasks($status: TaskStatus) {
+    tasks(status: $status){
+      id
+      title
+      status
+    }
+}
+`;
 
 const IndexPage: NextPage<Props, InitialProps> = props => {
     return <div>{props.greeting}</div>
